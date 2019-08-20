b1b856202074144ed6ae7c30fba1bb9eb225ebfe
akira-sasaki <gclue.akira@gmail.com>
2019-08-20 12:37:53 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ classifiers = ['Development Status :: 4 - Beta',
 
 setup(
     name         = 'FaBoColor_S11059',
-    version      = '1.0.1',
+    version      = '1.0.2',
     author       = 'FaBo',
     author_email = 'info@fabo.io',
     description  = "This is a library for the FaBo Color I2C Brick.",
